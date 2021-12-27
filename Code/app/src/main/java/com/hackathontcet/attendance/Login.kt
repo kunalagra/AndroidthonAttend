f326402207a1b119e9de27b6f439294f527aed5a
DEEXITH <deexithmadas772@gmail.com>
2021-12-27 15:01:11 +0530

    updated login and signup .kt files

--- Code/app/src/main/java/com/hackathontcet/attendance/Login.kt
+++ Code/app/src/main/java/com/hackathontcet/attendance/Login.kt
@@ -1,11 +1,32 @@
 package com.hackathontcet.attendance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class Login : AppCompatActivity() {
+
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var login: Button
+    private lateinit var signup: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        login = findViewById(R.id.login)
+        signup = findViewById(R.id.signup)
+
+        signup.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }

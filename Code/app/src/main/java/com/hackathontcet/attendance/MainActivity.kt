67365c31a8727c0bf2232972f7bf5eda927824da
kunalrag <goku.agra@yahoo.in>
2021-12-28 04:10:03 +0530

    Revert MainActivity As default launched in Manifest Skeleton made of Main Activity

--- Code/app/src/main/java/com/hackathontcet/attendance/MainActivity.kt
+++ Code/app/src/main/java/com/hackathontcet/attendance/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.hackathontcet.attendance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val login: Button = findViewById(R.id.loginb)
+        val signup: Button = findViewById(R.id.signupb)
+        signup.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+        login.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }

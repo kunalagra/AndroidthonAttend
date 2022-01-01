942c33eddec606d57bb8821e77680fe3bfb2d751
Ganesh Utla <ganeshutla888@gmail.com>
2022-01-01 10:42:10 +0530

    Updated the Profile

--- Code/app/src/main/java/com/hackathontcet/attendance/ProfileHomeScreen.kt
+++ Code/app/src/main/java/com/hackathontcet/attendance/ProfileHomeScreen.kt
@@ -1,12 +1,17 @@
 package com.hackathontcet.attendance
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileHomeScreen : AppCompatActivity() {
+
+    lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +23,19 @@ class ProfileHomeScreen : AppCompatActivity() {
             startActivity(Intent(this@ProfileHomeScreen, HomeScreenMainActivity::class.java))
             Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
         }
+
+//        loadProfile()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun loadProfile(){
+        val name = findViewById<TextView>(R.id.username)
+        val email = findViewById<TextView>(R.id.email)
+        val currentName = findViewById<TextView>(R.id.profile_name_text)
+        val currentEmail = findViewById<TextView>(R.id.profile_email_text)
+
+        currentName.text = "Username: "+name.text
+        currentEmail.text = "Username: "+email.text
+
     }
 }

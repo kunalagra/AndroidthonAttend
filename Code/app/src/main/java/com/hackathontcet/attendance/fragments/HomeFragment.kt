6e4e28d261c69c282d4ca10563c98f487d9e0980
Ganesh Utla <ganeshutla888@gmail.com>
2022-01-03 09:26:53 +0530

    Added Fragments

--- Code/app/src/main/java/com/hackathontcet/attendance/fragments/HomeFragment.kt
+++ Code/app/src/main/java/com/hackathontcet/attendance/fragments/HomeFragment.kt
@@ -1,60 +1,76 @@
 package com.hackathontcet.attendance.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.hackathontcet.attendance.R
+import com.hackathontcet.attendance.RecyclerAdapter
+import com.hackathontcet.attendance.Subjects
+import kotlinx.android.synthetic.main.fragment_home.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
+    lateinit var Rid : Array<Int>
+    lateinit var Sname : Array<String>
+
+    private var database = FirebaseDatabase.getInstance("https://attendance-c5215-default-rtdb.asia-southeast1.firebasedatabase.app")
+
+    private lateinit var newRecyclerView: RecyclerView
+    private lateinit var newArrayList: ArrayList<Subjects>
+    private lateinit var mAuth: FirebaseAuth
+
+    lateinit var imageId : Array<Int>
+    lateinit var name : Array<String>
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        imageId = arrayOf(
+            R.drawable.phy_icon,
+            R.drawable.chem_icon,
+            R.drawable.math_icon
+        )
+
+        name = arrayOf(
+            "Physics",
+            "Chemistry",
+            "Mathematics"
+        )
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        newRecyclerView = view.findViewById(R.id.rv_view)
+        newRecyclerView.layoutManager = LinearLayoutManager(this.context)
+        newRecyclerView.setHasFixedSize(true)
+        newArrayList = arrayListOf<Subjects>()
+        getUserdata()
+    }
+
+    private fun getUserdata() {
+        for (i in imageId.indices) {
+            val subject = Subjects(imageId[i], name[i])
+            Log.i("getUserdata", "$subject")
+            newArrayList.add(subject)
+        }
+        val adapter = RecyclerAdapter(newArrayList)
+        newRecyclerView.adapter = adapter
     }
 }
